82bc37064ddb0e62acc901a8d12e4f54e1d328db
NASADeveloper102 <NASADeveloper102@gmail.com>
2020-07-18 16:40:00 -0400

    Fixed many bugs

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 class VectorFunction{
-	constructor(xFunction, yFunction, zFunction, Name, Color, Size){
+	constructor(xFunction, yFunction, zFunction, Name, Color, Size, Bounds){
 		this.xFunction = xFunction;
 		this.yFunction = yFunction;
 		this.zFunction = zFunction;
 		this.Name = Name;
 		this.Color = Color;
 		this.Size = Size;
+		this.Bounds = Bounds;
 	}
 	CalculateValue(t){
 		return new p5.Vector(
@@ -41,11 +42,11 @@ var Rotate = true;
 function drawAxis(){
 	strokeWeight(5);
 	stroke(255, 0, 0);
-	line(0, 0, 400, 0, 0, -400);
+	line(0, 0, 300, 0, 0, -300);
 	stroke(0, 255, 0);
-	line(0, 400, 0, 0, -400, 0);
+	line(0, 300, 0, 0, -300, 0);
 	stroke(0, 0, 255);
-	line(400, 0, 0, -400, 0, 0);
+	line(300, 0, 0, -300, 0, 0);
 }
 function onRotationClick(){
 	RotationObject.x = parseFloat($('#xRotation').val());
@@ -59,14 +60,20 @@ function onCreateEquationClick(){
 	
 	strokeWeight(5);
 	Functions.push(new VectorFunction(
-		new Function("return " + $("#x").val())(), new Function("return " + $("#y").val())(), new Function("return " + $("#z").val())(), $('#name').val(), $('#color').val().toLowerCase(), $('#size').val()
+		new Function("return " + $("#x").val())(), 
+		new Function("return " + $("#y").val())(), 
+		new Function("return " + $("#z").val())(), 
+		$('#name').val(),
+		$('#color').val().toLowerCase(), 
+		$('#size').val(),
+		parseInt($('#bounds').val())
 	));
 	console.log($("#z").val());
 	$('#Equations').append('<div class="equation"><h3>' + $('#name').val() + '</h3></div>');
 }
 function onCreateButtonClick(){
 	SelectedWindow = Windows.Equations;
-	$("#EditTable").html("<h6>Name</h6><input id = 'name'><h6>Size of points</h6><input id = 'size' value='5'><h6>Color</h6><input id = 'color' value='Green'><h6>X</h6><textarea id ='x'>function x(t){return 50;}</textarea><h6>Y</h6><textarea id ='y'>function y(t){return 50;}</textarea><h6>Z</h6><textarea id ='z'>function z(t){return 50;}</textarea><br><button id = 'Create'>Create</button>");
+	$("#EditTable").html("<h6>Name</h6><input id = 'name'><h6>Bounds of function</h6><input id = 'bounds'><h6>Size of points</h6><input id = 'size' value='5' type ='number'><h6>Color</h6><input id = 'color' value='Green'><h6>X</h6><textarea id ='x'>function x(t){return 50;}</textarea><h6>Y</h6><textarea id ='y'>function y(t){return 50;}</textarea><h6>Z</h6><textarea id ='z'>function z(t){return 50;}</textarea><br><button id = 'Create'>Create</button>");
 	document.getElementById("Create").onclick = onCreateEquationClick;
 }
 function RotateUsingObject(){
@@ -84,7 +91,7 @@ function DeleteAll(){
 	$('#Equations').html("<h1>Equations</h1>");
 }
 function setup(){
-	createCanvas(windowWidth * 0.4, windowHeight * 0.6, WEBGL);
+	createCanvas(windowWidth * 0.4, windowHeight * 0.85, WEBGL);
 	document.getElementById("defaultCanvas0").onclick = onCanvasClick;
 	document.getElementById("CreateButton").onclick = onCreateButtonClick;
 	document.getElementById("DeleteButton").onclick = DeleteAll;
@@ -94,9 +101,11 @@ function drawPoints(){
 		stroke(Functions[i].Color);
 		strokeWeight(Functions[i].Size);
 		var PreviousValue = Functions[i].CalculateValue(-Math.round(AmountOfPoints/2));
-		for(var o = -Math.round(AmountOfPoints/2)+1; o < Math.round(AmountOfPoints/2); o++){
-			var Value = Functions[i].CalculateValue(o);
+		var PointValues = -25;
+		for(var o = -Math.round(AmountOfPoints/2); o < Math.round(AmountOfPoints/2); o++){
+			var Value = Functions[i].CalculateValue(PointValues);
 			point(Value.x, -Value.y, Value.z);
+			PointValues += (Functions[i].Bounds * 2)/AmountOfPoints;
 		}
 	}
 }
@@ -108,7 +117,7 @@ function draw(){
 }
 
 function windowResized(){
-	resizeCanvas(windowWidth * 0.4, windowHeight * 0.6);
+	resizeCanvas(windowWidth * 0.4, windowHeight * 0.85);
 }
 function onToggle(){
 	Rotate = !Rotate;
